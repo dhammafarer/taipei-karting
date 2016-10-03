e3e7fe938829f50c020abd8ee3f528154b38460b
Pawel Lisewski <pawel.lisewski@gmail.com>
2016-10-03 16:27:29 +0800

    Set up Races Show with absolute positioning on race circuit

--- resources/assets/js/components/RacesShow.vue
+++ resources/assets/js/components/RacesShow.vue
@@ -7,44 +7,52 @@
       <races-edit-dropdown :menu.sync="menu"></races-edit-dropdown>
 
       <section v-if="$route.name === 'races.show'" class="Race__Cover">
-        <div class="Race__Cover-photo" :style="{ backgroundImage: racePhoto }">
-          <div class="Race__Mask">
-            <div class="container">
 
-              <div class="Race__Birthdays">
-                <div class="Race__Birthday-driver" v-for="driver in birthdayDrivers">
-                  <a href="#" v-link="{ name: 'drivers.show', params: { id: driver.id } }">
-                    <span class="icon-birthday"></span>
-                    <img :src="driver.photo | driverThumb" :alt="driver.name">
-                  </a>
-                </div>
+        <div class="Race__Cover-photo"
+          :style="{ backgroundImage: full === true ? racePhoto : raceThumb }"
+          :class="{ 'blur': !full }"
+        >
+        </div>
+        <img @load="full = true" style="display: none" :src="race.photo | racePhoto">
+
+        <div class="Race__Mask">
+          <div class="container">
+
+            <div class="Race__Birthdays">
+              <div class="Race__Birthday-driver" v-for="driver in birthdayDrivers">
+                <a href="#" v-link="{ name: 'drivers.show', params: { id: driver.id } }">
+                  <span class="icon-birthday"></span>
+                  <img :src="driver.photo | driverThumb" :alt="driver.name">
+                </a>
+              </div>
+            </div>
+
+            <div class="Race__Edit">
+              <button class="Btn Btn--reverse" @click="openMenu">Edit Race</button>
+            </div>
+
+            <div class="col-sm-6 Race__Details">
+
+              <span class="Race__Name">{{ race.name }}</span>
+
+              <div class="Race__Date">
+                <span class="icon-calendar">{{ race.date | monthDay | tba }}</span>
+                <span class="icon-clock">{{ race.time | tba }}</span>
+                <span class="icon-location">{{ race.venue | capitalize | tba }}</span>
               </div>
 
-              <div class="Race__Edit">
-                <button class="Btn Btn--reverse" @click="openMenu">Edit Race</button>
+            </div><!-- Race Details -->
+
+            <div class="Race__Circuit" v-if="race.circuit">
+              <div class="Race__Circuit-photo" :style="{ backgroundImage: circuitPhoto }">
+                <span class="Race__Circuit-type">Track {{ race.circuit }}</span>
               </div>
-
-              <div class="col-sm-6 Race__Details">
-
-                <span class="Race__Name">{{ race.name }}</span>
-
-                <div class="Race__Date">
-                  <span class="icon-calendar">{{ race.date | monthDay | tba }}</span>
-                  <span class="icon-clock">{{ race.time | tba }}</span>
-                  <span class="icon-location">{{ race.venue | capitalize | tba }}</span>
-                </div>
-
-                <div v-if="race.circuit" class="Race__Circuit" :style="{ backgroundImage: circuitPhoto }">
-                  <span class="Race__Circuit-type">Track {{ race.circuit }}</span>
-                </div>
-
-              </div><!-- Race Details -->
+            </div>
 
 
-            </div><!-- container -->
+          </div><!-- container -->
 
-          </div><!-- Race__Mask -->
-        </div><!-- Race__Cover-Photo -->
+        </div><!-- Race__Mask -->
       </section>
 
       <div class="container">
@@ -88,12 +96,16 @@
     },
     data () {
       return {
+        full: false,
         menu: false
       }
     },
     computed: {
       racePhoto () {
         return 'url(' + this.$options.filters.racePhoto(this.race.photo) + ')'
+      },
+      raceThumb () {
+        return 'url(' + this.$options.filters.raceThumb(this.race.photo) + ')'
       },
       circuitPhoto () {
         return 'url(' + this.$options.filters.circuitPhoto(this.race.circuit) + ')'
